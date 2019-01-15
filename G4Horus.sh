005caa0e64728c37b2e3eb958c3785a7ee1fd728
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2019-01-15 16:14:05 +0100

    Update to Geant4 10.5, use modern cmake as far as possible

--- G4Horus.sh
+++ G4Horus.sh
@@ -4,11 +4,11 @@
 # On some systems, using the following option can improve
 # the 3D performance of the detector visualization.
 # On others, it will crash the Geant4 window.
-# export LIBGL_ALWAYS_INDIRECT=y
+export LIBGL_ALWAYS_INDIRECT=y
 
 ## Geant 4 Version
 # If a specific Geant4 version should be used, adapt and uncomment these lines.
-source /data/jmayer/software/geant4.10.04.p01/bin/geant4.sh
+source /data/jmayer/software/geant4.10.05/bin/geant4.sh
 echo "Using Geant4 Version `geant4-config --version` in `dirname $(dirname $(geant4-config --prefix))`"
 
 ## Build Project
